fff570515829aea6637338133ce578743a165591
nikolai <105597411+nkkls@users.noreply.github.com>
2024-04-19 09:52:40 +0800

    spinny glungus

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,17 @@ use sdl2::ttf;
 use std::thread;
 use std::time::Duration;
 
-fn lerp(start: i32, end: i32, t: f32) -> u32 {
-    (start as f32 + t * (end - start) as f32) as u32
+use rand::prelude::*;
+
+fn lerp(start: i32, end: i32, t: f32) -> i32 {
+    (start as f32 + t * (end - start) as f32) as i32
 }
 pub fn main() {
-    let window_width = 500;
-    let window_height = 500;
+    let window_width = 640;
+    let window_height = 480;
+
+    let glungus_clickwidth = 250;
+    let glungus_idlewidth = 300;
 
     let ctx = sdl2::init().unwrap();
     let video = ctx.video().unwrap();
@@ -31,22 +36,27 @@ pub fn main() {
     // IMPORTANT. KEEP GLUNGUS ALIVE.
     let texture_creator = canvas.texture_creator();
     let glungus = texture_creator
-        .load_texture_bytes(include_bytes!("glungus.png"))
+        .load_texture_bytes(include_bytes!("assets/glungus.png"))
         .unwrap();
 
     // IMPORTANT. CONTAIN GLUNGUS.
-    let mut container = Rect::new(0, 0, 400, 400);
+    let mut container = Rect::new(0, 0, glungus_idlewidth as u32, glungus_idlewidth as u32);
 
     // IMPORTANT. DATA RESEARCH
     let mut clicked: u32 = 0;
     let mut t: f32;
 
+    let mut glangle: f64 = 0.0; // BEST NAME EVER
+    let mut target_angle: f64 = 0.0;
+
+    let mut rng = thread_rng();
+
     // IMPORTANT. DATA RENDERING
     let ttfctx = ttf::init().unwrap();
-    let font_bytes = RWops::from_bytes(include_bytes!("Lexend-Bold.ttf"))
+    let font_bytes = RWops::from_bytes(include_bytes!("assets/Lexend-Bold.ttf"))
         .expect("can't create a RWops from Lexend-Bold.ttf");
     let font = ttfctx.load_font_from_rwops(font_bytes, 128).unwrap();
-    let text_container = sdl2::rect::Rect::new(0, 0, window_width, 50);
+    let text_container = sdl2::rect::Rect::new(10, 5, 170, 50);
 
     let mut event_poll = ctx.event_pump().unwrap();
     let mut debounce = false;
@@ -65,6 +75,7 @@ pub fn main() {
                 } => {
                     if !debounce {
                         clicked += 1;
+                        target_angle = rng.gen_range(-35..35) as f64;
                     }
                     debounce = true;
                 }
@@ -84,19 +95,30 @@ pub fn main() {
             t = 0.0;
             if t < 1.0 {
                 t += 0.1; // Adjust this value to control the speed of the movement.
-                container.set_width(lerp(container.width() as i32, 300, t));
-                container.set_height(lerp(container.height() as i32, 300, t));
+                container.set_width(lerp(container.width() as i32, glungus_clickwidth, t) as u32);
+                container.set_height(lerp(container.height() as i32, glungus_clickwidth, t) as u32);
+                if clicked % 20 == 0 {
+                    glangle = 360.0;
+                } else {
+                    glangle = lerp(glangle as i32, target_angle as i32 , t) as f64;
+                }
             }
         } else {
             t = 0.0;
             if t < 1.0 {
                 t += 0.1; // Adjust this value to control the speed of the movement.
-                container.set_width(lerp(container.width() as i32, 380, t));
-                container.set_height(lerp(container.height() as i32, 380, t));
+                container.set_width(lerp(container.width() as i32, glungus_idlewidth, t) as u32);
+                container.set_height(lerp(container.height() as i32, glungus_idlewidth, t) as u32);
+                glangle = lerp(glangle as i32, 0, t) as f64;
             }
         }
 
-        container.set_x(((window_width - container.width()) / 2).try_into().unwrap());
+        container.set_x(
+            ((window_width - container.width()) / 2)
+                .try_into()
+                .unwrap()
+        );
+
         container.set_y(
             ((window_height - container.height()) / 2)
                 .try_into()
@@ -105,11 +127,11 @@ pub fn main() {
 
         // RENDER GLUNGUS.
         canvas
-            .copy(&glungus, None, Some(container))
+            .copy_ex(&glungus, None, Some(container), glangle, None, false, false)
             .expect("GLUNGUS?? FAILED 2 RENDER?? WHAT");
 
         let text = font
-            .render(&format!("you have pressed the glungus {} times", clicked))
+            .render(&format!("-> {} times", clicked))
             .blended(Color::RGBA(255, 255, 255, 255))
             .unwrap();
 
